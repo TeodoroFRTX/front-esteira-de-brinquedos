cddcc081d75b1e4aa978fb5444587073d6ba559d
Teodoro Franco Teixeira <teodoro.teixeira>
2026-09-17 15:59:49 -0300

    vite colocado e atualizado

---
+++ esteira-gamer/src/main.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>toy factor new</h1>
+
+
+
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
